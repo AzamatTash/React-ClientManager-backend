6f85b4141716ccd62c3ffe1fc2a437b25f8d7d54
Azamat Tashinov <azamat300698@gmail.com>
2023-07-14 22:37:36 +0500

    fix index routes

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const router = new Router();
 const userRouter = require('./userRouter');
 const eventRouter = require('./eventRouter');
 const clientRouter = require('./clientRouter');
+const serverRouter = require('./serverRouter');
 
 router.use('/user', userRouter);
 router.use('/clients', clientRouter);
 router.use('/events', eventRouter);
+router.use('/ping', serverRouter);
 
 module.exports = router;
