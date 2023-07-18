e31df30ea1d249dcdcba95206917a25cf9ccea37
Azamat Tashinov <azamat300698@gmail.com>
2023-07-18 17:22:16 +0500

    remove router

--- routes/serverRouter.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express');
-const router = new Router();
-
-router.get('/', (req, res) => {
-	return res.json({ message: 'server is running', isLoading: false });
-});
-
-module.exports = router;
